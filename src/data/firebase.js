66f5745dcabf0da9b4eeee116ac04ba3ca4aea1c
Khrystyna Koval <hrustikovna@gmail.com>
2023-04-16 15:58:09 -0500

    added firebase.js

---
+++ src/data/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBGfO12Ki0UqnL04HabrddtAZArkeFglCA",
+  authDomain: "hrustikovna-rateadoctorapp.firebaseapp.com",
+  projectId: "hrustikovna-rateadoctorapp",
+  storageBucket: "hrustikovna-rateadoctorapp.appspot.com",
+  messagingSenderId: "856293726540",
+  appId: "1:856293726540:web:084f1d85327b6c137457e9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
